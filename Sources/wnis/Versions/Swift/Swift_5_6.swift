313769efa76b9e8c5a42fe7937412ed12f7a0bc5
Zach Eriksen <zeriksen@fnoex.com>
2021-08-10 11:18:25 -0500

    Updated 5.6

--- Sources/wnis/Versions/Swift/Swift_5_6.swift
+++ Sources/wnis/Versions/Swift/Swift_5_6.swift
@@ -4,10 +4,20 @@ struct Swift_5_6: SwiftVersion {
     var changes: [SwiftChange] {
         [
             SwiftChange(
-                name: "",
-                evolutionID: "SE-####",
-                description: "",
-                examples: []
+                name: "Unavailability Condition",
+                evolutionID: "SE-0290",
+                description: "Swift historically supported the #available condition to check if a specific symbol is available for usage, but not the opposite. In this proposal, we'll present cases where checking for the unavailability of something is necessary, the ugly workaround needed to achieve it today and how a new #unavailable condition can fix it.",
+                examples: [
+                    SwiftCodeExample(
+                        code: """
+                        if #unavailable(iOS 15.0) {
+                            // Old functionality
+                        } else {
+                            // iOS 15 functionality
+                        }
+                        """
+                    )
+                ]
             ),
         ]
     }
